474c755e34e05cd1e76d6c64da951c4881c67ac9
Lokesh Kumar <lokeshlkr@gmail.com>
2024-08-09 12:11:01 +0530

    created Https server

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+const fs = require('fs');
+const https = require('https');
+
+const privateKey = fs.readFileSync(process.env.HOME + '/.site/privatekey', 'utf8');
+const certificate = fs.readFileSync(process.env.HOME + '/.site/cert', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const PORT = 443
+
+
+
 app.use(express.static(path.join(__dirname, 'frontend', 'dist', 'browser')))
 
 app.all("/api", (req, res) => {
@@ -10,6 +21,9 @@ app.all("/api", (req, res) => {
 app.all("*", (req, res) => {
     res.sendFile(path.join(__dirname, 'frontend', 'dist', 'browser', 'index.html'))
 })
-app.listen(80, "0.0.0.0", () => {
-    console.log("Listening on port 80.");
+
+
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT, "0.0.0.0", () => {
+    console.log("Server is live now! Listening on port:", PORT);
 });
